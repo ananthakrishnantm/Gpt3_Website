55f567b900b70f77e1a1361cceeb9921820aa9df
Ananthakrishnan <t.m.ananthakrishnan@gmail.com>
2023-11-26 15:26:12 +0530

    Added footer and information section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./possibility.css";
-import PossibilityImage from "../../assets/possibility.png";
+import possibilityimage from "../../assets/possibility.png";
 
 const Possibility = () => {
   return (
     <div className="gpt3__possibility section_padding" id="possibility">
       <div className="gpt3__possibility-image">
-        <img src={PossibilityImage} alt="possibility image" />
+        <img src={possibilityimage} alt="possibilityimage" />
       </div>
       <div className="gpt3__possibility-content">
         <h4>Request Early Access to Get Started</h4>
